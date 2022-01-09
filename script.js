dd216193441ba7dd97302661ff61c0b130d239d8
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:09:06 +0500

    js

--- script.js
+++ script.js
@@ -480,3 +480,25 @@ console.log(hours_without_zeroes(dt));
 
 dt = new Date(1989, 10, 1); 
 console.log(hours_without_zeroes(dt));
+
+
+
+
+
+// Write a JavaScript function to get minutes with leading zeros (00 to 59).   
+// Test Data :
+// dt = new Date(1989, 10, 1);
+// console.log(minutes_with_leading_zeros(dt));
+// "00"
+
+
+function minutes_with_leading_zeros(dt) 
+{ 
+  return (dt.getMinutes() < 10 ? '0' : '') + dt.getMinutes();
+}
+
+dt = new Date(); 
+console.log(minutes_with_leading_zeros(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(minutes_with_leading_zeros(dt));
