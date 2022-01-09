0aee56d85d6060fed4fb7f4e5073cc61dd0ea789
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:36:00 +0500

    js

--- script.js
+++ script.js
@@ -939,3 +939,25 @@ console.log(internet_time(dt));
 
 dt = new Date(1989, 10, 1); 
 console.log(internet_time(dt));
+
+
+
+
+
+// Write a JavaScript function to get 12-hour format of an hour with leading zeros.   
+// Test Data :
+// dt = new Date(1989, 10, 1);
+// console.log(hours_with_zeroes(dt));
+// "12"
+
+function hours_with_zeroes(dt) 
+{ 
+  return ((dt.getHours() % 12 || 12) < 10 ? '0' : '') + (dt.getHours() % 12 || 12);
+}
+
+dt = new Date(); 
+console.log(hours_with_zeroes(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(hours_with_zeroes(dt));
+
