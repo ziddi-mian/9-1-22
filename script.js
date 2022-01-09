9cf0743136406c48641003e65b800523cf443602
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:16:32 +0500

    js

--- script.js
+++ script.js
@@ -574,3 +574,24 @@ console.log(lower_am_pm(dt));
   
   dt = new Date(1989, 10, 1); 
   console.log(upper_am_pm(dt));
+
+
+
+
+
+    //   Write a JavaScript function to swatch Internet time (000 through 999).   
+// Test Data :
+// dt = new Date(1989, 10, 1);
+// console.log(internet_time(dt));
+// 812
+
+function internet_time(dt) 
+{ 
+  return Math.floor((((dt.getUTCHours() + 1) % 24) + dt.getUTCMinutes() / 60 + dt.getUTCSeconds() / 3600) * 1000 / 24);
+}
+
+dt = new Date(); 
+console.log(internet_time(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(internet_time(dt));
