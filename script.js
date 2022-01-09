2eb2ae1b7e4952ed94a317805e14909db7513760
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:01:28 +0500

    js

--- script.js
+++ script.js
@@ -79,3 +79,22 @@ var getDaysInMonth = function(month,year) {
    console.log(getDaysInMonth(9, 2012));
    console.log(getDaysInMonth(12, 2012));
 
+
+
+
+   // program 4
+//   Write a JavaScript function to get the month name from a particular date.   
+
+// Test Data :
+// console.log(month_name(new Date("10/11/2009")));
+// console.log(month_name(new Date("11/13/2014")));
+// Output :
+// "October"
+// "November"
+  
+var month_name = function(dt){
+    mlist = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+      return mlist[dt.getMonth()];
+    };
+    console.log(month_name(new Date("10/11/2009")));
+    console.log(month_name(new Date("11/13/2014")));
