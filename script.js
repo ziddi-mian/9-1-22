d9e987d77a0ac6aa4251048344873775b348b76c
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:19:04 +0500

    js

--- script.js
+++ script.js
@@ -618,3 +618,25 @@ function calculate_age(dob) {
 console.log(calculate_age(new Date(1982, 11, 4)));
 
 console.log(calculate_age(new Date(1962, 1, 1)));
+
+
+
+
+
+// Write a JavaScript function to get the day of the month, 2 digits with leading zeros.   
+// Test Data :
+// d= new Date(2015, 10, 1);
+// console.log(day_of_the_month(d));
+// "01"
+
+
+function day_of_the_month(d)
+{ 
+  return (d.getDate() < 10 ? '0' : '') + d.getDate();
+}
+
+d= new Date();
+console.log(day_of_the_month(d));
+
+d= new Date(2015, 10, 1);
+console.log(day_of_the_month(d));
