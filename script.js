86b6739d8cba00db94fa9480c05fb9247f299ded
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:41:04 +0500

    js

--- script.js
+++ script.js
@@ -1189,3 +1189,26 @@ console.log(add_months(dt, 10).toString());
 
 dt = new Date(2014,10,2);
 console.log(add_months(dt, 10).toString());
+
+
+
+
+// Write a JavaScript function to get time differences in minutes between two dates.   
+// Test Data :
+// dt1 = new Date("October 13, 2014 11:11:00");
+// dt2 = new Date("October 13, 2014 11:13:00");
+// console.log(diff_minutes(dt1, dt2));
+// 2
+
+function diff_minutes(dt2, dt1) 
+ {
+
+  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= 60;
+  return Math.abs(Math.round(diff));
+  
+ }
+
+dt1 = new Date(2014,10,2);
+dt2 = new Date(2014,10,3);
+console.log(diff_minutes(dt1, dt2));
