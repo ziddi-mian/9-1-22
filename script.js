70f81c80400db1a6d49675f3d0cb2f9e60d2f5ee
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:40:47 +0500

    hsjh

--- script.js
+++ script.js
@@ -1165,3 +1165,27 @@ console.log(add_weeks(dt, 10).toString());
 
 dt = new Date(2014,10,2);
 console.log(add_weeks(dt, 10).toString());
+
+
+
+
+
+
+// Write a JavaScript function to add specified months to a date.   
+// Test Data :
+// dt = new Date(2014,10,2);
+// console.log(add_months(dt, 10).toString());
+// Output :
+// "Wed Sep 02 2015 00:00:00 GMT+0530 (India Standard Time)"
+
+function add_months(dt, n) 
+ {
+
+   return new Date(dt.setMonth(dt.getMonth() + n));      
+ }
+
+dt = new Date();
+console.log(add_months(dt, 10).toString());  
+
+dt = new Date(2014,10,2);
+console.log(add_months(dt, 10).toString());
