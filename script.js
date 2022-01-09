6e9cfef877eab1766ec1ffeaa71c6802c73af33e
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:01:53 +0500

    js

--- script.js
+++ script.js
@@ -98,3 +98,30 @@ var month_name = function(dt){
     };
     console.log(month_name(new Date("10/11/2009")));
     console.log(month_name(new Date("11/13/2014")));
+
+
+
+
+     // program 5
+
+//     Write a JavaScript function to compare dates (i.e. greater than, less than or equal to).   
+
+// Test Data :
+// console.log(compare_dates(new Date('11/14/2013 00:00'), new Date('11/14/2013 00:00')));
+// console.log(compare_dates(new Date('11/14/2013 00:01'), new Date('11/14/2013 00:00')));
+// console.log(compare_dates(new Date('11/14/2013 00:00'), new Date('11/14/2013 00:01')));
+// Output :
+// "Date1 = Date2"
+// "Date1 > Date2"
+// "Date2 > Date1"
+
+
+var compare_dates = function(date1,date2){
+    if (date1>date2) return ("Date1 > Date2");
+  else if (date1<date2) return ("Date2 > Date1");
+  else return ("Date1 = Date2"); 
+ }
+
+console.log(compare_dates(new Date('12/18/2021 00:00'), new Date('12/18/2021 00:00')));
+console.log(compare_dates(new Date('12/18/2021 00:01'), new Date('12/18/2021 00:00')));
+console.log(compare_dates(new Date('12/18/2021 00:00'), new Date('12/18/2021 00:01')));
