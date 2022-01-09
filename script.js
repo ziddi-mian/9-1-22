8e0a9673f95109818603af3fd89a2cbdc5e166c0
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:03:19 +0500

    js

--- script.js
+++ script.js
@@ -193,3 +193,27 @@ var date_diff_indays = function(date1, date2) {
     }
     console.log(date_diff_indays('04/02/2014', '11/04/2014'));
     console.log(date_diff_indays('12/02/2014', '11/04/2014'));
+
+
+
+
+     //     program 9
+
+//     Write a JavaScript function to get the last day of a month.   
+
+// Test Data :
+// console.log(lastday(2014,0));
+// console.log(lastday(2014,1));
+// console.log(lastday(2014,11));
+// Output :
+// 31
+// 28
+// 31
+
+var date_diff_indays = function(date1, date2) {
+    dt1 = new Date(date1);
+    dt2 = new Date(date2);
+    return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+    }
+    console.log(date_diff_indays('04/02/2014', '11/04/2014'));
+    console.log(date_diff_indays('12/02/2014', '11/04/2014'));
