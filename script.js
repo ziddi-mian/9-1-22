04d461d1737bfd51e8f198b8eeea8e9fef630e30
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:38:04 +0500

    js

--- script.js
+++ script.js
@@ -1042,3 +1042,39 @@ console.log(seconds_with_leading_zeros(dt));
 
 dt = new Date(1989, 10, 1); 
 console.log(seconds_with_leading_zeros(dt));
+
+
+
+
+// Write a JavaScript function to find whether or not the date is in daylights savings time.   
+// Test Data :
+// dt = new Date();
+// console.log(daylights_savings(dt));
+// 1
+
+function daylights_savings(dt) 
+{ 
+  var dst = null;
+  for (var i = 0; i < 12; ++i) 
+    {
+      var d = new Date(dt.getFullYear(), i, 1);
+      var offset = dt.getTimezoneOffset();
+
+      if (dst === null) 
+        dst = offset;
+      else if (offset < dst) 
+         {
+           dst = offset; 
+           break;
+         } 
+      else if (offset > dst) 
+        break;
+         }
+return (dt.getTimezoneOffset() == dst) | 0;
+}
+
+dt = new Date(); 
+console.log(daylights_savings(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(daylights_savings(dt));
