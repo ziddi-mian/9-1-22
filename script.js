bc9c9bbf44d9d8548dc2fc9608e0b99ba12c296e
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:38:45 +0500

    js

--- script.js
+++ script.js
@@ -1099,3 +1099,25 @@ console.log(diff_to_GMT(dt));
 
 dt = new Date(1989, 10, 1); 
 console.log(diff_to_GMT(dt));
+
+
+
+
+
+// Write a JavaScript function to get timezone offset in seconds.   
+// Note : The offset for timezones west of UTC is always negative, and for those east of UTC is always positive.
+// Test Data :
+// dt = new Date();
+// console.log(timezone_offset_in_seconds(dt));
+// 19800
+
+function timezone_offset_in_seconds(dt) 
+{ 
+   return -dt.getTimezoneOffset() * 60;
+}
+
+dt = new Date(); 
+console.log(timezone_offset_in_seconds(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(timezone_offset_in_seconds(dt));
