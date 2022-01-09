3a247712018267d18e5ccaad1c33d2491df792d0
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:00:24 +0500

    js

--- script.js
+++ script.js
@@ -51,3 +51,31 @@ var curday = function(ap){
     };
     console.log(curday('/'));
     console.log(curday('-'));
+
+
+
+      //  progrm 3
+
+//     Write a JavaScript function to get the number of days in a month.   
+
+// Test Data :
+// console.log(getDaysInMonth(1, 2012));
+// console.log(getDaysInMonth(2, 2012));
+// console.log(getDaysInMonth(9, 2012));
+// console.log(getDaysInMonth(12, 2012));
+// Output :
+// 31
+// 29
+// 30
+// 31
+
+var getDaysInMonth = function(month,year) {
+
+    return new Date(year, month, 0).getDate();
+ 
+   };
+   console.log(getDaysInMonth(1, 2012));
+   console.log(getDaysInMonth(2, 2012));
+   console.log(getDaysInMonth(9, 2012));
+   console.log(getDaysInMonth(12, 2012));
+
