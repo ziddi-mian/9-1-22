4a0c8d84b2178e31a52e376da7a998b1ae448612
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:00:01 +0500

    js

--- script.js
+++ script.js
@@ -22,3 +22,32 @@
     
   }
   calculator(1,2,'+')
+
+
+
+
+  
+    // program 2
+
+//   Write a JavaScript function to get the current date.   
+
+// Note : Pass a separator as an argument.
+// Test Data :
+// console.log(curday('/'));
+// console.log(curday('-'));
+// Output :
+// "11/13/2014"
+// "11-13-2014"
+
+var curday = function(ap){
+    today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    
+    if(dd<10) dd='0'+dd;
+    if(mm<10) mm='0'+mm;
+    return (mm+ap+dd+ap+yyyy);
+    };
+    console.log(curday('/'));
+    console.log(curday('-'));
