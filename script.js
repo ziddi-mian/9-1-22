2a23f08659c867d441bf1c224ff0786913ef0fa1
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:42:00 +0500

    js

--- script.js
+++ script.js
@@ -1266,3 +1266,33 @@ console.log(diff_days(dt1, dt2));
 dt1 = new Date("October 13, 2014 08:11:00");
 dt2 = new Date("October 19, 2014 11:13:00");
 console.log(diff_days(dt1, dt2));
+
+
+
+
+
+// Write a JavaScript function to get time differences in weeks between two dates.   
+// Test Data :
+// dt1 = new Date("June 13, 2014 08:11:00");
+// dt2 = new Date("October 19, 2014 11:13:00");
+// console.log(diff_weeks(dt1, dt2));
+// 18
+
+
+function diff_weeks(dt2, dt1) 
+ {
+
+  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= (60 * 60 * 24 * 7);
+  return Math.abs(Math.round(diff));
+  
+ }
+
+dt1 = new Date(2014,10,2);
+dt2 = new Date(2014,10,11);
+console.log(diff_weeks(dt1, dt2));
+
+dt1 = new Date("June 13, 2014 08:11:00");
+dt2 = new Date("October 19, 2014 11:13:00");
+console.log(diff_weeks(dt1, dt2));
+
