968bb5daa2bc13a3c44ce07a77d65e4f43a9db2a
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:04:24 +0500

    js

--- script.js
+++ script.js
@@ -268,3 +268,31 @@ function max_date(all_dates) {
      return max_dt;
       }
     console.log(max_date(['2015/02/01', '2015/02/02', '2015/01/03']));
+
+
+
+
+
+    // program11
+
+// Write a JavaScript function to get the maximum date from an array of dates. Go to the editor
+
+// Test Data :
+// console.log(max_date(['2015/02/01', '2015/02/02', '2015/01/03']));
+// Output :
+// "2015/02/02"
+
+function max_date(all_dates) {
+    var max_dt = all_dates[0],
+      max_dtObj = new Date(all_dates[0]);
+    all_dates.forEach(function(dt, index)
+      {
+      if ( new Date( dt ) > max_dtObj)
+      {
+      max_dt = dt;
+      max_dtObj = new Date(dt);
+      }
+      });
+     return max_dt;
+      }
+    console.log(max_date(['2015/02/01', '2015/02/02', '2015/01/03']));
