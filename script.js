d78718fb4a58edd0576002bc653aebc77d9ef338
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:02:12 +0500

    js

--- script.js
+++ script.js
@@ -125,3 +125,20 @@ var compare_dates = function(date1,date2){
 console.log(compare_dates(new Date('12/18/2021 00:00'), new Date('12/18/2021 00:00')));
 console.log(compare_dates(new Date('12/18/2021 00:01'), new Date('12/18/2021 00:00')));
 console.log(compare_dates(new Date('12/18/2021 00:00'), new Date('12/18/2021 00:01')));
+
+
+
+
+// programk 6
+
+// Write a JavaScript function to add specified minutes to a Date object.   
+
+// Test Data :
+// console.log(add_minutes(new Date(2014,10,2), 30).toString());
+// Output :
+// "Sun Nov 02 2014 00:30:00 GMT+0530 (India Standard Time)"
+
+var add_minutes =  function (dt, minutes) {
+    return new Date(dt.getTime() + minutes*60000);
+}
+console.log(add_minutes(new Date(2020,11,18), 30).toString());
