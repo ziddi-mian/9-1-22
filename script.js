db021d021ae199eaec20b0ed2e981f13168c6a9a
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:24:38 +0500

    js

--- script.js
+++ script.js
@@ -888,3 +888,20 @@ console.log(sort_year(dt));
 
 dt = new Date(1989, 10, 1); 
 console.log(sort_year(dt));
+
+
+
+
+
+// Write a JavaScript function to get lowercase Ante meridiem and Post meridiem.
+
+function lower_am_pm(dt) 
+{ 
+   return dt.getHours() < 12 ? 'AM' : 'PM';
+}
+
+dt = new Date(); 
+console.log(lower_am_pm(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(lower_am_pm(dt));
