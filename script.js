9db1a9981fd3ab998f2af66c4a2801a222554d3f
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:18:50 +0500

    js

--- script.js
+++ script.js
@@ -595,3 +595,26 @@ console.log(internet_time(dt));
 
 dt = new Date(1989, 10, 1); 
 console.log(internet_time(dt));
+
+
+
+
+
+// Write a JavaScript program to calculate age.   
+
+// Test Data :
+// console.log(calculate_age(new Date(1982, 11, 4)));
+// 32
+// console.log(calculate_age(new Date(1962, 1, 1)));
+// 53
+
+function calculate_age(dob) { 
+    var diff_ms = Date.now() - dob.getTime();
+    var age_dt = new Date(diff_ms); 
+  
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
+
+console.log(calculate_age(new Date(1982, 11, 4)));
+
+console.log(calculate_age(new Date(1962, 1, 1)));
