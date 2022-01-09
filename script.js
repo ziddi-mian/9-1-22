bae13850ee936065e02f81c1afd621408abd0844
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:38:27 +0500

    js

--- script.js
+++ script.js
@@ -1078,3 +1078,24 @@ console.log(daylights_savings(dt));
 
 dt = new Date(1989, 10, 1); 
 console.log(daylights_savings(dt));
+
+
+
+
+
+// Write a JavaScript function to get difference to Greenwich time (GMT) in hours.   
+// Test Data :
+// dt = new Date();
+// console.log(diff_to_GMT(dt));
+// "+05.500"
+
+function diff_to_GMT(dt) 
+{ 
+   return (-dt.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(dt.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(dt.getTimezoneOffset() / 60)) + '00';
+}
+
+dt = new Date(); 
+console.log(diff_to_GMT(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(diff_to_GMT(dt));
