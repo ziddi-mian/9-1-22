6f6e03efb151c60077b6a50e71f34fd413f72d10
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:41:42 +0500

    js

--- script.js
+++ script.js
@@ -1239,3 +1239,30 @@ console.log(diff_hours(dt1, dt2));
 dt1 = new Date("October 13, 2014 08:11:00");
 dt2 = new Date("October 13, 2014 11:13:00");
 console.log(diff_hours(dt1, dt2));
+
+
+
+
+// Write a JavaScript function to get time differences in days between two dates.   
+// Test Data :
+// dt1 = new Date("October 13, 2014 08:11:00");
+// dt2 = new Date("October 19, 2014 11:13:00");
+// console.log(diff_days(dt1, dt2));
+// 6
+
+function diff_days(dt2, dt1) 
+ {
+
+  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= (60 * 60 * 24);
+  return Math.abs(Math.round(diff));
+  
+ }
+
+dt1 = new Date(2014,10,2);
+dt2 = new Date(2014,10,6);
+console.log(diff_days(dt1, dt2));
+
+dt1 = new Date("October 13, 2014 08:11:00");
+dt2 = new Date("October 19, 2014 11:13:00");
+console.log(diff_days(dt1, dt2));
