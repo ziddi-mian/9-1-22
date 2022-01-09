06f860e85b508921b80e6d0ec9d7decc99ea734d
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:19:19 +0500

    js

--- script.js
+++ script.js
@@ -640,3 +640,24 @@ console.log(day_of_the_month(d));
 
 d= new Date(2015, 10, 1);
 console.log(day_of_the_month(d));
+
+
+
+
+// Write a JavaScript function to get the day of the month, 2 digits with leading zeros.   
+// Test Data :
+// d= new Date(2015, 10, 1);
+// console.log(day_of_the_month(d));
+// "01"
+
+
+function day_of_the_month(d)
+{ 
+  return (d.getDate() < 10 ? '0' : '') + d.getDate();
+}
+
+d= new Date();
+console.log(day_of_the_month(d));
+
+d= new Date(2015, 10, 1);
+console.log(day_of_the_month(d));
