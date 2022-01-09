198e50b9b46fa3e93578305259daf4b65816dd3e
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-09 11:11:12 +0500

    js

--- script.js
+++ script.js
@@ -522,3 +522,25 @@ console.log(seconds_with_leading_zeros(dt));
 
 dt = new Date(1989, 10, 1); 
 console.log(seconds_with_leading_zeros(dt));
+
+
+
+
+
+
+// Write a JavaScript function to get Timezone.   
+// Test Data :
+// dt = new Date();
+// console.log(seconds_with_leading_zeros(dt));
+// "India Standard Time"
+
+function seconds_with_leading_zeros(dt) 
+{ 
+  return /\((.*)\)/.exec(new Date().toString())[1];
+}
+
+dt = new Date(); 
+console.log(seconds_with_leading_zeros(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(seconds_with_leading_zeros(dt));
